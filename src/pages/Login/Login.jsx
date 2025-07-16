da3376c8f6ecbb15ef623c621cf9290a70c5f6af
Pedro <pedrohenrique010123456@gmail.com>
2025-07-16 20:25:49 -0300

    começo do login e preguiça pra continuar

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
+//usuário/email, senha, botão, confirmação de login(usuário/email e senha corretos), navigate para home
+
 export const Login = () => {
-  return <h1>Login</h1>;
+  const [ususario, setUsuario] = useState(null);
+  const [email, setEmail] = useState(null);
+  const login = (usuario, email, senha) => {
+    if (
+      usuario === "adimin" ||
+      (email === "adimin@teste.com" && senha === "123")
+    ) {
+      setUser({ user });
+    } else {
+      alert("Usuário ou senha incorretos");
+    }
+  };
 };
