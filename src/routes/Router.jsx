819c7867e42adbcd81f6197120e8504adb7596f8
Pedro <pedrohenrique010123456@gmail.com>
2025-07-17 19:33:02 -0300

    criação do layout, navbar e rotas filhas com outlet para carrinho e outras coisas

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,15 +3,20 @@ import { Home } from "../pages/Home/Home";
 import { Login } from "../pages/Login/Login";
 import { Cadastro } from "../pages/Cadastro/Cadastro";
 import { Edicao } from "../pages/Edicao/Edicao";
+import { Layout } from "../components/Layout/Layout";
+import { Carrinho } from "../pages/Carrinho/Carrinho";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/edicao/:id" element={<Edicao />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/edicao/:id" element={<Edicao />} />
+          <Route path="/carrinho" element={<Carrinho />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
