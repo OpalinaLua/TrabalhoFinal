21e4cd558bd8b200abb3c20f55ef9c27ee8b0bcc
Simone Raeder Blasse Gorni <PET0099063@aluno.firjansenaisesi.com.br>
2025-07-17 18:20:46 -0300

    footer e header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <>
         <footer className={styles.footer}>
             <p> Mini Loja Todos os direitos reservados</p>
-            <p>⛅ Confira a previsão do tempo para compras inteligentes! ➡️ <a href="https://www.climatempo.com.br/previsao-do-tempo/15-dias/cidade/317/petropolis-rj" target='_blank'> 🌡️ </a></p>
+            <p>⛅ Confira a previsão do tempo para compras inteligentes!! ➡️ <a href="https://www.climatempo.com.br/previsao-do-tempo/15-dias/cidade/317/petropolis-rj" target='_blank'> 🌡️ </a></p>
         </footer>
         </>
     );
