b1c1eead94e622bef16e446d322d870f3362f2e8
Pedro <pedrohenrique010123456@gmail.com>
2025-07-21 19:33:47 -0300

    remoção de um import inutilizado

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import { Home } from "../pages/Home/Home";
 import { Login } from "../pages/Login/Login";
 import { Cadastro } from "../pages/Cadastro/Cadastro";
