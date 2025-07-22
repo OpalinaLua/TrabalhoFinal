54237361d0220ecc8a65165478abe9d3023e7467
OpalinaLua <Luamdarabello@gmail.com>
2025-07-22 18:47:56 -0300

    resetcss e coemco do css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,49 @@
 import { House, ShoppingCart } from "lucide-react";
 import { NavLink } from "react-router";
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
   return (
     <>
-      <NavLink
-        to="/"
-        className={({ isActive }) => (isActive ? "link-ativo" : "")}
-      >
-        {({ isActive }) =>
-          isActive ? (
-            <div
-              style={{
-                display: "flex",
-              }}
-            >
-              <House /> Início
-            </div>
-          ) : (
-            "Início"
-          )
-        }
-      </NavLink>
-      <NavLink
-        to="/carrinho"
-        className={({ isActive }) => (isActive ? "link-ativo" : "")}
-      >
-        {({ isActive }) =>
-          isActive ? (
-            <div
-              style={{
-                display: "flex",
-              }}
-            >
-              <ShoppingCart />
-              Carrinho
-            </div>
-          ) : (
-            "Carrinho"
-          )
-        }
-      </NavLink>
+      <div className={styles.conteiner}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "link-ativo" : "")}
+        >
+          {({ isActive }) =>
+            isActive ? (
+              <div
+                style={{
+                  display: "flex",
+                }}
+              >
+                <House /> Início
+              </div>
+            ) : (
+              "Início"
+            )
+          }
+        </NavLink>
+        <NavLink
+          to="/carrinho"
+          className={({ isActive }) => (isActive ? "link-ativo" : "")}
+        >
+          {({ isActive }) =>
+            isActive ? (
+              <div
+                style={{
+                  display: "flex",
+                }}
+              >
+                <ShoppingCart />
+                Carrinho
+              </div>
+            ) : (
+              "Carrinho"
+            )
+          }
+        </NavLink>
+      </div>
     </>
   );
 };
