e5e7ed4b4c6d137891f69823a48d1ac36152bbfa
OpalinaLua <Luamdarabello@gmail.com>
2025-07-22 19:38:34 -0300

    cartao criado comeco css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,12 @@
-import { useEffect, useState } from "react";
-import { buscarProdutos } from "../../service/apiService";
-
+import { Cartao } from "../../components/Cartao/Cartao";
+import styles from "./Home.module.css";
 export const Home = () => {
-  const [produtos, setProdutos] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const getData = async () => {
-    try {
-      const data = await buscarProdutos();
-      setProdutos(data);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  if (loading) return <h1>Carregando...</h1>;
-
   return (
-    <>
-      <h1>Home</h1>
-      {produtos.map((produto) => (
-        <p key={produto.id}>{produto.nome}</p>
-      ))}
-    </>
+    <div className={styles.home}>
+      <>
+        <h1>Home</h1>
+        <Cartao />
+      </>
+    </div>
   );
 };
