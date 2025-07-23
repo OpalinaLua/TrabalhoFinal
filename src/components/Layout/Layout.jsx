e881805c6290390c934b5368dcceaebf849146ac
Pedro <pedrohenrique010123456@gmail.com>
2025-07-23 18:54:49 -0300

    carrinho funcionando com o add cart e botão de adicionar ao carrinho no card

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,18 @@
 import { Outlet } from "react-router";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
-import styles from './Layout.module.css';
+import styles from "./Layout.module.css";
 
 export const Layout = ({ onSearch, onClear }) => {
   return (
     <>
-    <div className={styles.pageContainer}>
-    <div className={styles.content}>
-    <Header onClear={onClear} onSearch={onSearch} />
-      <h1 className={styles.h1}>Tudo pra facilitar seu dia a dia</h1>
-      <p className={styles.p}>O que você precisa está aqui. Peça e receba onde estiver.</p>
-      </div>
-    
-      <Outlet />
-      <Footer />
+      <div className={styles.pageContainer}>
+        <div className={styles.content}>
+          <Header onClear={onClear} onSearch={onSearch} />
+        </div>
+
+        <Outlet />
+        <Footer />
       </div>
     </>
   );
