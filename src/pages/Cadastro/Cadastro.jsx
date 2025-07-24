645ab0e5b8722beb7ebaaef0522fcdd7db5b8637
OpalinaLua <Luamdarabello@gmail.com>
2025-07-24 19:13:29 -0300

    css cadastro completo

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { criarProdutos } from "../../service/apiService";
+import { FormularioP } from "../../components/FormularioP/FormularioP";
+import styles from "./Cadastro.module.css";
 
 export const Cadastro = () => {
   const navegacao = useNavigate();
@@ -29,14 +31,16 @@ export const Cadastro = () => {
 
   return (
     <div>
-      <button onClick={() => navegacao("/")}>Voltar</button>
-      <h1>Novo Produto</h1>
+      <h1 className={styles.titulo}>Novo Produto</h1>
       <FormularioP
         produtos={produto}
         onChange={handleChange}
         onSubmit={handleSubmit}
         isChanged={isChanged}
       />
+      <button className={styles.botao} onClick={() => navegacao("/")}>
+        Voltar
+      </button>
     </div>
   );
 };
